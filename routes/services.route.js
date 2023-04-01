c2c4b4b0eeadb92a92f988ab4c0f669217ee6473
TH-Raju <rajukhan894200@gmail.com>
2023-04-01 17:56:55 +0600

    code review

--- routes/services.route.js
+++ routes/services.route.js
@@ -2,10 +2,6 @@ const express = require('express')
 const router = express.Router();
 const carController = require("../controller/services.controller.js");
 
-// router.get('/', (req, res) => {
-//     res.send("route found")
-// })
-
 router.route('/').get(carController.getServices)
 router.route('/:id').get(carController.getSingleServices)
 
